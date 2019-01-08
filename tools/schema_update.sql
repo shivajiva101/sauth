3f007dcc5887edfd105aa71c8953b8e0b1ae0298
shivajiva101 <shivajiva101@hotmail.com>
2019-01-08 18:01:39 +0000

    Update schema_update.sql

--- tools/schema_update.sql
+++ tools/schema_update.sql
@@ -2,13 +2,14 @@ PRAGMA foreign_keys=off;
 
 BEGIN TRANSACTION;
 
-ALTER TABLE auth RENAME auth_temp;
+ALTER TABLE auth RENAME TO auth_temp;
 CREATE TABLE IF NOT EXISTS auth (name VARCHAR (32) PRIMARY KEY ON CONFLICT IGNORE, password VARCHAR (512), privileges VARCHAR (512), last_login INTEGER);
 INSERT INTO auth SELECT name, password, privileges, last_login FROM auth_temp;
 DROP TABLE auth_temp;
 
-ALTER TABLE _s ADD COLUMN db_version VARCHAR (6);
-INSERT INTO _s (db_version) VALUES ('1.1');
+DROP TABLE _s;
+CREATE TABLE _s (import BOOLEAN, db_version VARCHAR (6));
+INSERT INTO _s VALUES ('true', '1.1');
 
 COMMIT;
  
